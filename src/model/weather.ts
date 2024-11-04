6cc7d521f61a3703e44aaa6a3790e9fd1b9d620b
biswajitshipyaari <biswajit.behura@shipyaari.com>
2024-11-04 14:17:53 +0530

    sell

--- src/model/weather.ts
+++ src/model/weather.ts
@@ -1,25 +1,17 @@
 import mongoose, { Schema, Document } from "mongoose";
 
 interface Iweather extends Document {
-  location: string;
-  temperature: string;
-  description: string;
-  icon: string;
-  date: number;
+  date?: number;
 }
 
 const weatherSchema: Schema = new Schema(
   {
-    location: { type: String, default: "" },
-    temperature: { type: Number, default: "" },
-    description: { type: String, default: "" },
-    icon: { type: String, default: "" },
-    date: { type: Date, default: Date.now },
+    date: { type: Number, default: Date.now() },
   },
   {
     timestamps: true,
     versionKey: false,
-    strict: true,
+    strict: false,
   }
 );
 
--- src/service/weatherService.ts
+++ src/service/weatherService.ts
@@ -4,19 +4,43 @@ import { config } from "../config/configEnv";
 
 export const getWeather = async (req: any) => {
   try {
-    const { lat, lon } = req;
-    const response = await axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall/overview?lat=${lat}&lon=${lon}&appid=${config.WEATHER_API_KEY}`
+    const { location } = req;
+    let response = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${config.WEATHER_API_KEY}
+`
+    );
+    if (!response?.data?.[0]?.lat && !response?.data?.[0]?.lon) {
+      return {
+        success: false,
+        message: "Weather data not found",
+        statusCode: 404,
+      };
+    }
+    response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${response?.data?.[0]?.lat}&lon=${response?.data?.[0]?.lon}&appid=${config.WEATHER_API_KEY}`
     );
     console.log(response.data);
-    const weatherData = {
-      location,
-      temperature: response.data.main.temp,
-      description: response.data.weather?.[0]?.description,
-      icon: response.data.weather?.[0]?.icon,
-    };
 
-    const newWeather = await weatherSchema.create(weatherData);
+    if (!response.data) {
+      return {
+        success: false,
+        message: "Weather data not found",
+        statusCode: 404,
+      };
+    }
+    // const weatherData = {
+    //   location,
+    //   temperature: response.data?.main?.temp,
+    //   description: response.data.weather?.[0]?.description,
+    //   icon: response.data.weather?.[0]?.icon,
+    //   visibility: response?.data?.visibility,
+    //   wind: response?.data?.wind,
+    // };
+
+    const newWeather = await weatherSchema.create({
+      location,
+      ...response?.data,
+    });
 
     return {
       success: true,
@@ -36,15 +60,25 @@ export const getWeather = async (req: any) => {
 export const getWeatherHistory = async (req: any) => {
   try {
     const { location, startDate, endDate } = req;
-
-    const history = await weatherSchema.find({
-      location,
-      date: { $gte: new Date(startDate), $lte: new Date(endDate) },
-    });
+    // Set default startDate to one week ago if it's not provided
+    const defaultStartDate = new Date();
+    defaultStartDate.setDate(defaultStartDate.getDate() - 7); // Set to 7 days ago
+    defaultStartDate.setHours(0, 0, 0, 0); // Set to start of the day
+    // Set default endDate to today if it's not provided
+    const today = new Date();
+    today.setHours(23, 59, 59, 999); // Set to end of today
+    const history = await weatherSchema
+      .find({
+        location: { $regex: location, $options: "i" }, // Case-insensitive search
+        createdAt: {
+          $gte: startDate ? new Date(startDate) : defaultStartDate,
+          $lte: endDate ? new Date(endDate) : today,
+        },
+      })
+      .lean();
     if (!history?.length) {
-      throw new Error("Email does not exists");
+      throw new Error("Weather does not exists");
     }
-
     return {
       success: true,
       data: history,
